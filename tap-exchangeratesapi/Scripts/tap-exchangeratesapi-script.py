60e1c30ce00f3b063c9c4696d4c951d07d173dc3
ahmad humayun <ahmadhumayun.k@gmail.com>
2022-07-26 19:24:21 +0500

    Added Code

---
+++ tap-exchangeratesapi/Scripts/tap-exchangeratesapi-script.py
@@ -0,0 +1,33 @@
+#!E:\courses\currency_exchange_test\tap-exchangeratesapi\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'tap-exchangeratesapi==0.1.1','console_scripts','tap-exchangeratesapi'
+import re
+import sys
+
+# for compatibility with easy_install; see #2198
+__requires__ = 'tap-exchangeratesapi==0.1.1'
+
+try:
+    from importlib.metadata import distribution
+except ImportError:
+    try:
+        from importlib_metadata import distribution
+    except ImportError:
+        from pkg_resources import load_entry_point
+
+
+def importlib_load_entry_point(spec, group, name):
+    dist_name, _, _ = spec.partition('==')
+    matches = (
+        entry_point
+        for entry_point in distribution(dist_name).entry_points
+        if entry_point.group == group and entry_point.name == name
+    )
+    return next(matches).load()
+
+
+globals().setdefault('load_entry_point', importlib_load_entry_point)
+
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(load_entry_point('tap-exchangeratesapi==0.1.1', 'console_scripts', 'tap-exchangeratesapi')())
